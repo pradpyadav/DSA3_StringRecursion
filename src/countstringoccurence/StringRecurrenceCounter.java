500e73ef972520b8c2bbcded486b15a165956be2
Pradeep Yadav <pradpyadav@gmail.com>
2024-07-27 15:39:07 -0400

    Program to calculate occurrence of a String in a given string

--- src/countstringoccurence/StringRecurrenceCounter.java
+++ src/countstringoccurence/StringRecurrenceCounter.java
@@ -14,33 +14,30 @@ public class StringRecurrenceCounter {
         System.out.println("Enter targetString: ");
         String targetString = scan.next();
 
-        System.out.println("Input String : "+inputString);
-        System.out.println("Target String : "+targetString);
+        System.out.println("Input String : " + inputString);
+        System.out.println("Target String : " + targetString);
 
-        System.out.println("Calculating string occurence value : "+countStringOccurence(inputString,targetString));
+        System.out.println("Calculating string occurence value : " + countStringOccurence(inputString, targetString));
 
     }
 
-    static  int countStringOccurence(String inputString, String targetString){
-      return countstringOccurenceHelper(inputString,targetString,0);
+    static int countStringOccurence(String inputString, String targetString) {
+        return countstringOccurenceHelper(inputString, targetString, 0);
     }
 
     static int countstringOccurenceHelper(String inputString, String targetString, int idx) {
 
         //Base Condition to check if the index has gone past the threshhold length
-        if(idx > inputString.length()-targetString.length())
+        if (idx > inputString.length() - targetString.length())
             return 0;//this means we didnot get any string match
 
-        int subStringCountResult = countstringOccurenceHelper(inputString,targetString,idx+1);
+        int subStringCountResult = countstringOccurenceHelper(inputString, targetString, idx + 1);
 
         //comparing the starting string for equality if eaual add it to th eresult else return the result
-        boolean doStartStringMatch = inputString.substring(idx, idx+targetString.length()).equals(targetString);
-        if(doStartStringMatch)
-            return subStringCountResult+1;
+        boolean doStartStringMatch = inputString.substring(idx, idx + targetString.length()).equals(targetString);
+        if (doStartStringMatch)
+            return subStringCountResult + 1;
         else
             return subStringCountResult;
-
     }
-
-
 }
